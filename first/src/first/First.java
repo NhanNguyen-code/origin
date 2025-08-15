072707cbcde6f7700db04de29ce7757bd2013422
NhanNguyen-code <nguyennhanpro47@gmail.com>
2025-08-15 15:04:21 +0700

    first

---
+++ first/src/first/First.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package first;
+
+/**
+ * 
+ */
+public class First {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Nguyen Thanh Nhan");
+
+	}
+
+}
